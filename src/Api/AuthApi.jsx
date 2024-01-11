8f4fecf32fbd5ce7c43ff9d431b7fea1d5dc0175
RojuGhimire <ghimireroju@gmail.com>
2024-01-11 14:42:35 +0545

    login

---
+++ src/Api/AuthApi.jsx
@@ -0,0 +1,6 @@
+// AuthApi.jsx
+
+export const LoginApi = () => {
+    return "hello From Login Api"
+  };
+  
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-import './index.css'
-import {
-  
-  RouterProvider,
-} from "react-router-dom";
-import {router} from './Routes'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'; // Use BrowserRouter or HashRouter here
+import { router } from './Routes';
+import { app } from './FirebaseConfig';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <RouterProvider router={router} /> {/* Replace RouterProvider with BrowserRouter or HashRouter */}
+    </BrowserRouter>
+  </React.StrictMode>
+);
